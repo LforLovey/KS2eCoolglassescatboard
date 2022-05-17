60eaf99efa2ee96ee55285534633a0b1a43ff58c
LforLovey <93688568+LforLovey@users.noreply.github.com>
2022-05-17 00:07:23 -0400

    Added CAN

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,29 @@ void setup() {
   // put your setup code here, to run once:
   delay(5000);
   FanSpeed=255;
+  CAN.begin();
+  CAN.setBaudRate(500000);
+  CAN.enableFIFO();
+  CAN.enableFIFOInterrupt();
+  CAN.onReceive(canSniff);
+  CAN.mailboxStatus();
+}
+
+void canSniff(const CAN_message_t &msg) {
+  Serial.print("MB "); Serial.print(msg.mb);
+  Serial.print("  OVERRUN: "); Serial.print(msg.flags.overrun);
+  Serial.print("  LEN: "); Serial.print(msg.len);
+  Serial.print(" EXT: "); Serial.print(msg.flags.extended);
+  Serial.print(" TS: "); Serial.print(msg.timestamp);
+  Serial.print(" ID: "); Serial.print(msg.id, HEX);
+  Serial.print(" Buffer: ");
+  for ( uint8_t i = 0; i < msg.len; i++ ) {
+    Serial.print(msg.buf[i], HEX); Serial.print(" ");
+  } Serial.println();
 }
 
 void loop() {
+  CAN.events();
 
   for(int i=0;i<8;i++){
     analogWrite(i,FanSpeed);
